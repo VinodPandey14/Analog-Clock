de60e59b343a69cf72106bb6697df2384bc89865
Vinod Pandey <itsvinod14@gmail.com>
2024-02-17 14:10:10 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let hr = document.querySelector("#hour");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+let audio = document.querySelector("#tic-tic");
+let btn = document.querySelector("button");
+let clk = document.querySelector(".clock");
+let value = 0;
+
+const displayTime = () => {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  let hRotation = 30 * hh + mm / 2;
+  let mRotation = 6 * mm;
+  let sRotation = 6 * ss;
+
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
+};
+
+const playAudio = () => {
+  audio.play();
+};
+
+setInterval(displayTime,1000);
+setInterval(playAudio,1000);
